f2ee0d4b53cbd310edf5d83355ee3396a9585371
Mustafa Önder ÜNLÜ <116373504+mustafaonderunluu@users.noreply.github.com>
2024-01-31 19:51:41 +0300

    Proje dosyası ekle.

---
+++ metot_two/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+
+//Metot Mantığı 
+
+//static void Main(string[] args)
+//{
+//    int x = 5;
+//    Console.WriteLine("X in önceki hali :" + x);
+//    artir( x);
+//    Console.WriteLine("X in sonraki hali :" + x);
+
+//    void artir( int x)
+//    {
+//        x++;
+//        Console.WriteLine("Metot İçinde ki hali :" + x);
+//    }
+//}
+
+//sayi rakamlarin toplamı (Metot değil)
+
+
+Console.WriteLine("Sayı giriniz : ");
+int sayi=Convert.ToInt32(Console.ReadLine());
+static int rakamlarınToplami(int sayi)
+{
+    int gecici = sayi;
+    int toplam = 0;
+    while (gecici != 0) {
+
+
+        int rakam = gecici % 10;
+        toplam += rakam;
+       gecici = gecici / 10;
+
+
+    }
+    return toplam;
+}
+Console.WriteLine(sayi + "Rakamların Toplami :" + rakamlarınToplami(sayi));
+Console.ReadLine();
